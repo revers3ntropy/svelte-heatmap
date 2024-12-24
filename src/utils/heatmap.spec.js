92490c8b578f300bce83b584395088a5bc79a3be
revers3ntropy <josephcoppin@gmail.com>
2024-12-24 12:31:41 +0000

    Add failing test case which generates an empty week in a chunk, which will cause a crash.

--- src/utils/heatmap.spec.js
+++ src/utils/heatmap.spec.js
@@ -1,4 +1,4 @@
-import { getCalendar } from './heatmap';
+import { chunkWeeks, getCalendar } from './heatmap';
 import { stringifyDate } from './date';
 
 describe('heatmap utils', () => {
@@ -58,6 +58,34 @@ describe('heatmap utils', () => {
             expect(calendar[4].value).toBe(5);
             expect(calendar[5].value).toBe(0);
             expect(calendar[6].value).toBe(0);
+        });
+
+        it('does not create an empty week', () => {
+            const calendar = getCalendar({
+                colors: [],
+                data: [
+                    { date: '2022-12-18T12:00:00', value: 1 },
+                    { date: '2022-12-19T12:00:00', value: 1 },
+                    { date: '2022-12-20T12:00:00', value: 1 },
+                    { date: '2022-12-21T12:00:00', value: 1 },
+                    { date: '2022-12-22T12:00:00', value: 1 },
+                ],
+                emptyColor: '#000',
+                startDate: '2022-12-24T11:00:00',
+                endDate: '2024-12-24T11:00:00',
+                view: 'weekly',
+            });
+
+            const chunks = chunkWeeks({
+                allowOverflow: false,
+                calendar,
+                startDate: '2022-12-24T11:00:00',
+                endDate: '2024-12-24T11:00:00'
+            })
+
+            for (const chunk of chunks) {
+                expect(chunk.length).toBeGreaterThan(0);
+            }
         });
     });
 });
